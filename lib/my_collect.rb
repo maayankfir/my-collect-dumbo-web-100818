83740d99c65ae014704aa83b7465d58f2739c935
Maayan Kfir <mayankfir@gmail.com>
2018-09-24 18:42:23 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,8 @@
-def my_collect(array)
-  my_collect(array) do |x|
-    x
+def my_collect(collection)
+  i = 0
+  while i < collection.length 
+    my_collect(collection) do |x| 
+      i += 1
+    end
   end
 end
